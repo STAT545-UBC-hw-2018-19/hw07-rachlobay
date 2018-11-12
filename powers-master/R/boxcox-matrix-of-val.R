56209616a43498a3ff83887a0c6b534885f25935
Rachel Lobay <rachel.lobay@stat.ubc.ca>
2018-11-11 17:20:33 -0800

    After creating boxcox matrix of values function.

---
+++ powers-master/R/boxcox-matrix-of-val.R
@@ -0,0 +1,24 @@
+#' Box-Cox Transform Matrix of Values Function
+#'
+#' This function is to be used in the boxcox.transform function to return a matrix of values that
+#' underwent a box-cox transform when lambda is a vector.
+#' Each column of the matrix corresponds to a different lambda (and is named according to the lambda value).
+#' I'm making it internal, because I don't want users to use it.
+#' Rather, I want to use it in the boxcoxtransform function.
+#'
+#' @param x vector that is to be transformed under the box-cox function.
+#' @param lambda vector of values that indicate the strength of the box-cox transformation
+#'
+#' @return a matrix of the `x` values that underwent box-cox transformation for different lambdas. The entries of a column correspond to a particular lambda value. Hence, the columns are named to reflect what lambda value was used.
+values_mat = matrix(rep(x,length(lambda)), ncol = length(lambda)) # define a values matrix
+colnames(values_mat) <- lambda # replace column names of values matrix by the corresponding lambda values
+
+# fill in the matrix of values with the box-cox transformed values for each lambda
+boxcox.tranform.mat <- function(x, lambda = 0) {
+    for(i in 1:length(lambda)){
+    if(lambda[i] != 0) values_mat[,i] = (x^lambda[i] - 1) / lambda[i]
+    else(values_mat[,i] = log(x))
+    }
+    return(values_mat)
+}
+
---
+++ powers-master/R/boxcoxtransform.R
@@ -0,0 +1,20 @@
+#' Box-Cox Transform Function
+#'
+#' Given a variable, the Box-Cox power transformation is obtained.
+#'
+#' @param x vector that is to be transformed under the box-cox function.
+#' @param lambda strength of the transformation. Default is 0. May be a single value or a vector for this function.
+#' @param makepos the constant value that should be added to each `x`  to make them positive. Default is 0.
+#'
+#' @return A vector of the values that have been transformed. If lambda is a 0 value, returns the `log(x)`. If lambda is non-0 value, returns `(x^lamba - 1) / lambda`. When lambda is a vector, a marrix of values that were transformed with each column of the matrix corresponds to a lambda.
+#' @export
+
+boxcox.transform <- function(x, lambda = 0, makepos = 0) {
+# Common error checks
+    if(any(x < 0)) stop("Negative x-value(s). Change makepos argument of function to add a constant to make all x values positive.")
+    if((makepos< 0)) stop("Nice try, but makepos value must be non-negative.")
+
+# Box-Cox Function Computations
+    if(lambda == 0) return(log(x))
+    else return (ifelse(is.vector(lambda), , (x^lambda - 1) / lambda))
+}
